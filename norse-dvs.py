75a5b72b6ed39177469d54c8b1682ca1fddd7caa
James Paul Turner <jamesturner246@gmail.com>
2020-12-03 18:32:02 +0000

    Simplified model. 3 blocks plus dense, and reduced feature channels.

--- norse-dvs.py
+++ norse-dvs.py
@@ -1,5 +1,4 @@
 import argparse
-import numpy as np
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
@@ -27,88 +26,53 @@ class DVSModel(pl.LightningModule):
         self.block1 = SequentialState(
             nn.Conv2d(3, 64, 3, padding=1, bias=False),
             LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            # nn.Conv2d(64, 64, 3, padding=1, bias=False),
-            # LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            nn.BatchNorm2d(64),
             nn.AvgPool2d(2, stride=2, ceil_mode=True),  # 1/2
+            nn.BatchNorm2d(64),
         )
 
         # block 2
         self.block2 = SequentialState(
             nn.Conv2d(64, 128, 3, padding=1, bias=False),
             LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            # nn.Conv2d(128, 128, 3, padding=1, bias=False),
-            # LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            nn.BatchNorm2d(128),
             nn.AvgPool2d(2, stride=2, ceil_mode=True),  # 1/4
+            nn.BatchNorm2d(128),
         )
 
         # block 3
         self.block3 = SequentialState(
             nn.Conv2d(128, 256, 3, padding=1, bias=False),
             LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            # nn.Conv2d(256, 256, 3, padding=1, bias=False),
-            # LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
             nn.AvgPool2d(2, stride=2, ceil_mode=True),  # 1/8
             nn.BatchNorm2d(256),
         )
 
-        # block 4
-        self.block4 = SequentialState(
-            nn.Conv2d(256, 512, 3, padding=1, bias=False),
-            LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            nn.BatchNorm2d(512),
-            nn.AvgPool2d(2, stride=2, ceil_mode=True),  # 1/16
-        )
-
-        self.block5 = SequentialState(
-            nn.Conv2d(512, 512, 3, padding=1, bias=False),
-            LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            nn.BatchNorm2d(512),
-            nn.AvgPool2d(2, stride=2, ceil_mode=True),  # 1/32
-        )
-
         # dense
         self.dense = SequentialState(
-            nn.Conv2d(512, 1024, 7, padding=3, bias=False),
+            nn.Conv2d(256, 512, 7, padding=3, bias=False),
             LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            # nn.Dropout2d(),
-            nn.Conv2d(1024, 1024, 1, bias=False),
+            nn.Conv2d(512, 512, 1, bias=False),
             LIFFeedForwardCell(p=LIFParameters(method=method, alpha=alpha), dt=dt),
-            nn.BatchNorm2d(1024),
-            # nn.Dropout2d(),
+            nn.BatchNorm2d(512),
         )
 
-        self.score_block3 = nn.Conv2d(256, n_class, 1, bias=False)
-        self.score_block4 = nn.Conv2d(512, n_class, 1, bias=False)
-        self.score_dense = nn.Conv2d(1024, n_class, 1, bias=False)
+        self.score_block2 = nn.Conv2d(128, n_class, 1, bias=False)
+        self.deconv_block2 = nn.ConvTranspose2d(n_class, n_class, 8, stride=4, padding=2, bias=False)
 
-        self.upscore_2 = nn.ConvTranspose2d(
-            n_class, n_class, 4, stride=2, padding=1, bias=False
-        )
-        self.upscore_4 = nn.ConvTranspose2d(
-            n_class, n_class, 4, stride=2, padding=1, bias=False
-        )
-        self.upscore_8 = nn.ConvTranspose2d(
-            n_class, n_class, 16, stride=8, padding=4, bias=False
-        )
+        self.score_dense = nn.Conv2d(512, n_class, 1, bias=False)
+        self.deconv_dense = nn.ConvTranspose2d(n_class, n_class, 16, stride=8, padding=4, bias=False)
 
         self.final = LIFeedForwardCell(dt=dt)
 
     def forward(self, x):
-
-        state_block1 = (
-            state_block2
-        ) = (
-            state_block3
-        ) = state_block4 = state_block5 = state_dense = state_final = None
+        state_block1 = state_block2 = state_block3 = state_dense = state_final = None
+        x = x.float()
 
         # output              batch,      class,        frame,      height,     width
         output = torch.empty((x.shape[0], self.n_class, x.shape[1], x.shape[3], x.shape[4]))
 
         # for each frame
         for i in range(x.shape[1]):
-            frame = x[:, i, :, :, :].float()
+            frame = x[:, i, :, :, :]
 
             for j in range(self.iter_per_frame):
 
@@ -117,28 +81,24 @@ class DVSModel(pl.LightningModule):
                 out_block1, state_block1 = self.block1(frame, state_block1)       # 1/2
                 out_block2, state_block2 = self.block2(out_block1, state_block2)  # 1/4
                 out_block3, state_block3 = self.block3(out_block2, state_block3)  # 1/8
-                out_block4, state_block4 = self.block4(out_block3, state_block4)  # 1/16
-                out_block5, state_block5 = self.block5(out_block4, state_block5)  # 1/32
-                out_dense, state_dense = self.dense(out_block5, state_dense)
+                out_dense, state_dense = self.dense(out_block3, state_dense)
 
                 self.log("out_block1_mean", out_block1.mean())
                 self.log("out_block2_mean", out_block2.mean())
                 self.log("out_block3_mean", out_block3.mean())
-                self.log("out_block4_mean", out_block4.mean())
-                self.log("out_block5_mean", out_block5.mean())
                 self.log("out_dense_mean", out_dense.mean())
 
                 ####### WITH FEATURE FUSION
-                out_score_block3 = self.score_block3(out_block3)  # 1/8
-                out_score_block4 = self.score_block4(out_block4)  # 1/16
-                out_score_dense = self.score_dense(out_dense)  # 1/32
+                out_score_block2 = self.score_block2(out_block2)
+                out_deconv_block2 = self.deconv_block2(out_score_block2)
 
-                out_upscore_2 = self.upscore_2(out_score_dense)  # 1/16
-                out_upscore_block4 = self.upscore_4(out_score_block4 + out_upscore_2)  # 1/8
-                out_upscore_8 = self.upscore_8(out_score_block3 + out_upscore_block4)  # 1/1
+                out_score_dense = self.score_dense(out_dense)
+                out_deconv_dense = self.deconv_dense(out_score_dense)
+
+                out_deconv = out_deconv_block2 + out_deconv_dense
                 #######
 
-                out_final, state_final = self.final(out_upscore_8, state_final)
+                out_final, state_final = self.final(out_deconv, state_final)
 
             output[:, :, i, :, :] = out_final
 
@@ -153,15 +113,14 @@ class DVSModel(pl.LightningModule):
         # We need: B, T, C, H, W
         x = x.permute(0, 1, 4, 2, 3)
 
-        # lightning moves all tensors to gpu, but we want to compute loss on cpu
-        self.loss_fn.to('cpu')  # move loss class weights back to cpu
-        y = y.to('cpu')         # move labels back to cpu
-
         z = self.forward(x)
 
-        # print(x.device)
-        # print(y.device)
-        # print(z.device)
+        # # compute loss on cpu
+        # self.loss_fn.to('cpu')
+        # y = y.to('cpu')
+
+        # or compute loss on gpu
+        z = z.to('cuda')
 
         loss = self.loss_fn(z, y)
         return loss
@@ -177,12 +136,14 @@ class DVSNRPDataset(torch.utils.data.Dataset):
         self.data = torch.load(file)
         self.skip_frames = skip_frames
 
-        # labels = torch.Tensor([d[1] for d in self.data])
+        # labels = torch.tensor([d[1] for d in self.data])
         # print('unique labels: ', torch.unique(labels))
         
     def __getitem__(self, index):
         data, labels = self.data[index]
-        return data[self.skip_frames:], labels[self.skip_frames:].astype(np.long)
+        data = torch.tensor(data[self.skip_frames:])
+        labels = torch.tensor(labels[self.skip_frames:], dtype=torch.long)
+        return data, labels
     
     def __len__(self):
         return len(self.data)
